526363bb47d73410ffe295b512545f65e61c3895
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2024-08-20 11:35:21 +0900

    successful UI test

--- UI.test.ts
+++ UI.test.ts
@@ -10,6 +10,7 @@ describe("UIクラス", () => {
     <div class="answerClass"></div>
     <button class="button"></button>
     <div class="resetBox"></div>
+    <button class="resetButton">リセット</button>
     `;
 
     game = new Game();
@@ -28,5 +29,37 @@ describe("UIクラス", () => {
     expect(answerClass).not.toBeNull();
     expect(button).not.toBeNull();
     expect(resetBox).not.toBeNull();
+  });
+
+  it("ボタンクリック時にカウントを正しくする", () => {
+    const button = document.querySelector(".button") as HTMLElement;
+    button.click();
+
+    const countDisplay = document.querySelector(".DisplayingTheCount");
+    expect(countDisplay).not.toBeNull();
+    expect(countDisplay?.innerHTML).toBe(`挑戦回数: 1`);
+  });
+
+  it("ユーザーの入力に応じて、基づくメッセージを表示する", () => {
+    const input = document.querySelector(".answerClass") as HTMLInputElement;
+    input.value = "50";
+    game.targetNumber = 50;
+
+    const button = document.querySelector(".button") as HTMLElement;
+    button.click();
+
+    const verbalReaction = document.querySelector(".verbalReaction");
+
+    expect(verbalReaction?.innerHTML).toBe("おめでとう！正解です！");
+  });
+
+  it("10回の試行後にリセットボタンを表示する", () => {
+    for (let i = 10; i < 10; i++) {
+      const button = document.querySelector(".button") as HTMLElement;
+      button.click();
+    }
+
+    const resetButton = document.querySelector(".resetButton");
+    expect(resetButton).not.toBeNull();
   });
 });
