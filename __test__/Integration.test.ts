f093e68f6a35ef970cbdacd0ba0b6ac401462018
CA01971164 <CA01971164@users.noreply.github.com>
2024-09-04 07:27:02 +0000

    deploy: c1d7e08698c21b6828135681b8e6a73d9aba109e

---
+++ __test__/Integration.test.ts
@@ -0,0 +1,33 @@
+import { Game, UI } from "../src/index";
+
+describe("GameとUIの結合テスト", () => {
+  let game: Game;
+  let ui: UI;
+
+  beforeEach(() => {
+    document.body.innerHTML = `
+        <div class="reactionBox"></div>
+        <div class="verbalReaction"></div>
+        <input class="answerClass" />
+        <button class="button"></button>
+        <div class="resetBox"></div>
+      `;
+
+    game = new Game();
+    ui = new UI(game);
+  });
+
+  it("リセットボタンがクリックされたときにゲームがリセットされる", () => {
+    for (let i = 10; i < 10; i++) {
+      const button = document.querySelector(".button") as HTMLElement;
+      button.click();
+    }
+
+    const resetButton = document.querySelector(".resetButton") as HTMLElement;
+    resetButton?.click();
+
+    expect(game.count).toBe(0);
+    expect(document.querySelector(`.DisplayingTheCount`)).toBeNull;
+    expect(document.querySelector(`.resetButton`)).toBeNull;
+  });
+});
